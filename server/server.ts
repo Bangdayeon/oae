3b1657a57a492a0e7d4e4c4883dcd2483bc219ce
방다연 <bangdayeon@bangdayeon-ui-Macmini.local>
2025-06-30 18:27:41 +0900

    add server for OAuth

---
+++ server/server.ts
@@ -0,0 +1,68 @@
+import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/auth/google', async (req, res) => {
+    const redirect_url: string = process.env.REDIRECT_URL || '';
+    const client_id: string = process.env.GOOGLE_CLIENT_ID || '';
+    const scope = encodeURIComponent('https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile');
+    const state = crypto.randomUUID();
+
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?` + 
+        `client_id=${client_id}&` + 
+        `redirect_uri=${redirect_url}&` +
+        `response_type=code&` +
+        `scope=${scope}&` +
+        `state=${state}&` +
+        `prompt=consent`;
+
+    res.redirect(authUrl);
+});
+
+app.get('/api/auth/callback/google', async (req, res) => {
+    const code = req.query.code as string;
+
+    try {
+        // 1. access_token 요청
+        const tokenResponse = await axios.post('https://oauth2.googleapis.com/token', {
+            code,
+            client_id: process.env.GOOGLE_CLIENT_ID || '',
+            client_secret: process.env.GOOGLE_CLIENT_SECRET || '',
+            redirect_uri: process.env.REDIRECT_URL || '',
+            grant_type: 'authorization_code',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
+
+        const {access_token, id_token} = tokenResponse.data;
+
+        // 2. 사용자 정보 요청
+        const userInfoResponse = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+            },
+        });
+
+        res.json(userInfoResponse.data);
+    } catch (error) {
+        console.error(error);
+        let errorMessage = 'Unknown error';
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+        res.status(500).json({
+            error: 'OAUth Failed', detail: errorMessage });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listeneing on http://localhost:${PORT}`);
+});
